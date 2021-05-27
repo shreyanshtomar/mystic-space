cb5bb5d4e690dfd284d0e1a7f37edb7a5c7ada9e
shreyansh <shreyanshtomar99@gmail.com>
2021-05-28 00:05:14 +0530

    broken homepage in components, finished styling menu items

---
+++ mystic-space/src/components/directory/directory.components.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl:
+            "https://images.unsplash.com/photo-1565839412225-98641b2d7b5b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+          id: 1,
+        },
+        {
+          title: "jackets",
+          imageUrl:
+            "https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
+          id: 2,
+        },
+        {
+          title: "sneakers",
+          imageUrl:
+            "https://images.unsplash.com/photo-1580813089076-7a92471e7074?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1124&q=80",
+          id: 3,
+        },
+        {
+          title: "womens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1596725649320-7f257a9da04b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+          size: "large",
+          id: 4,
+        },
+        {
+          title: "mens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1532789207428-eb0c63ae912a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
+          size: "large",
+          id: 5,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
