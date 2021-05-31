4bde9a4702bf1872e8d7a8b72d3a901d81ff42f0
shreyansh <shreyanshtomar99@gmail.com>
2021-05-31 15:32:43 +0530

    added react router DOM and hats route

--- mystic-space/src/components/directory/directory.components.jsx
+++ mystic-space/src/components/directory/directory.components.jsx
@@ -13,18 +13,21 @@ class Directory extends React.Component {
           imageUrl:
             "https://images.unsplash.com/photo-1565839412225-98641b2d7b5b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
           id: 1,
+          linkUrl: "hats",
         },
         {
           title: "jackets",
           imageUrl:
             "https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
           id: 2,
+          linkUrl: "",
         },
         {
           title: "sneakers",
           imageUrl:
             "https://images.unsplash.com/photo-1580813089076-7a92471e7074?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1124&q=80",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "womens",
@@ -32,6 +35,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1596725649320-7f257a9da04b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "mens",
@@ -39,6 +43,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1532789207428-eb0c63ae912a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -47,8 +52,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
