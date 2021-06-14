605af973979ae921ef947c57aa8adf4e53a4a2f4
shreyanshtomar <shreyanshtomar99@gmail.com>
2021-06-15 01:06:44 +0530

    added remove item from checkout page functionality to update the state in cartItem reducer

--- src/components/checkout-item/checkout-item.components.jsx
+++ src/components/checkout-item/checkout-item.components.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { clearItem } from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, quantity, price } }) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img src={imageUrl} alt='item' />
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { imageUrl, name, quantity, price } = cartItem;
+
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img src={imageUrl} alt='item' />
+      </div>
+
+      <span className='name'>{name}</span>
+      <span className='quantity'>{quantity}</span>
+      <span className='price'>${price}</span>
+      <span className='remove' onClick={() => clearItem(cartItem)}>
+        &#9932;
+      </span>
     </div>
+  );
+};
 
-    <span className='name'>{name}</span>
-    <span className='quantity'>{quantity}</span>
-    <span className='price'>${price}</span>
-    <span className='remove'>&#9932;</span>
-  </div>
-);
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItem(item)),
+});
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
