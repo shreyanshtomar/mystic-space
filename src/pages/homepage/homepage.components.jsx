b8ad77c749648a35526cf7d2852f54d317f8de13
shreyanshtomar <shreyanshtomar99@gmail.com>
2021-06-30 03:12:52 +0530

    Performance-Enhancements #5: Added Lazy Loading to Homepage

--- src/pages/homepage/homepage.components.jsx
+++ src/pages/homepage/homepage.components.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import Directory from '../../components/directory/directory.components';
-
+import React, { lazy, Suspense } from 'react';
+//import Directory from '../../components/directory/directory.components';
 import './homepage.styles.scss';
+
+const Directory = lazy(() =>
+  import('../../components/directory/directory.components')
+);
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
-      <Directory />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className='homepage'>
+        <Directory />
+      </div>
+    </Suspense>
   );
 };
 
